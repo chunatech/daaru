421fcd0fa60a1e67588b1f361a2045379ab7bfa5
Tina Colvin <tinamcolvin11@gmail.com>
2023-03-05 20:22:24 -0700

    remove recurseListOfDirectories. add timestamp formats for filename and logentries. Add method to get timestamp for current system time. Add ApplicationName param.

--- cWeed/Utils.fs
+++ cWeed/Utils.fs
@@ -1,4 +1,19 @@
 module Utils
 
+open System
 open System.IO
 
+
+/// formatter for DateTime obj that is specified to the milisecond 
+/// for use with timestamps in LogEntry
+let TimeStampDateFormat = @"yyyy/MM/dd HH:mm:ss.fff"
+
+/// formatter for DateTime obj that is specified to the second 
+/// for use with log files being rolled
+let FileNameDateFormat = @"yyyy/MM/dd HH:mm:ss"
+
+/// returns the name of the exe as a string
+let Applicationname = Reflection.Assembly.GetExecutingAssembly().GetName().Name
+
+/// this method creates a timestamp string formatted for LogEntry records
+let createTimeStampNow (datetime: DateTime) (fmt: string) = (datetime).ToString(fmt)
