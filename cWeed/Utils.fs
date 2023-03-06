205b871ccee385685b521ccbab195b9581699524
Tina Colvin <tinamcolvin11@gmail.com>
2023-03-06 11:00:13 -0700

    refactor: rename Applicatonname to ApplicationName and createTimeStampNow to CreateTimeStampNow

--- cWeed/Utils.fs
+++ cWeed/Utils.fs
@@ -13,7 +13,7 @@ let TimeStampDateFormat = @"yyyy/MM/dd HH:mm:ss.fff"
 let FileNameDateFormat = @"yyyy/MM/dd HH:mm:ss"
 
 /// returns the name of the exe as a string
-let Applicationname = Reflection.Assembly.GetExecutingAssembly().GetName().Name
+let ApplicationName = Reflection.Assembly.GetExecutingAssembly().GetName().Name
 
 /// this method creates a timestamp string formatted for LogEntry records
-let createTimeStampNow (datetime: DateTime) (fmt: string) = (datetime).ToString(fmt)
+let CreateTimeStampNow (datetime: DateTime) (fmt: string) = (datetime).ToString(fmt)
