0a0faf8a9f33152c35a047e7979c24015f310efd
Tina Colvin <tinamcolvin11@gmail.com>
2023-06-08 13:23:53 -0600

    log: add logging to Configuration.fs

--- cweed/Configuration.fs
+++ cweed/Configuration.fs
@@ -3,8 +3,9 @@ module Configuration
 open System.IO
 open Thoth.Json.Net
 
-open ConfigurationDecoders 
 open ConfigTypes
+open ConfigurationDecoders 
+open Logger
 
 (*
     TODO: 
@@ -13,26 +14,33 @@ open ConfigTypes
 
 
 /// default location of the user provided configuration file
-let DefaultConfigurationFileLocation = Path.Combine(System.AppContext.BaseDirectory, "/config")
+let DefaultConfigurationFileLocation = Path.Join(System.AppContext.BaseDirectory, "config")
+QueueLog (LogLevel.DEBUG) (System.Reflection.MethodBase.GetCurrentMethod()) ($"the value of DefaultConfigurationFileLocation is %s{DefaultConfigurationFileLocation}")
 
 /// default config file name
 let DefaultConfigurationFileName = "config.json"
-
+QueueLog (LogLevel.DEBUG) (System.Reflection.MethodBase.GetCurrentMethod()) ($"the value of DefaultConfigurationFileName is %s{DefaultConfigurationFileName}")
 /// load a configuration file from a filepath. If the file does not exist, then 
 /// load the default application configuration
 let ConfigurationFromFileOrDefault filepath =
     // if the configfile isn't there just return the defaults 
     if (File.Exists(filepath) |> not) then 
+        QueueLog (LogLevel.WARN) (System.Reflection.MethodBase.GetCurrentMethod()) ($"a configuration file was not found at %s{filepath}. using defaults")
         AppConfiguration.Default()
     else 
+        QueueLog (LogLevel.DEBUG) (System.Reflection.MethodBase.GetCurrentMethod()) ($"configuration file at %s{filepath} found. reading contents")
+        
         let contents: string = 
             File.ReadAllTextAsync(filepath) 
             |> Async.AwaitTask 
             |> Async.RunSynchronously
 
+        QueueLog (LogLevel.DEBUG) (System.Reflection.MethodBase.GetCurrentMethod()) "configuration file contents read. decoding contents..."
         match contents |> Decode.fromString AppConfigurationDecoder with 
-            | Ok config -> config
+            | Ok config -> 
+                QueueLog (LogLevel.DEBUG) (System.Reflection.MethodBase.GetCurrentMethod()) ($"app configuration was decoded successfully, %A{config}")
+                config
             | Error errstr -> 
-                printfn $"error loading configfile %s{errstr}"
+                QueueLog (LogLevel.WARN) (System.Reflection.MethodBase.GetCurrentMethod()) ($"error decoding configuration file at %s{filepath}. %s{errstr}. using defaults")
                 AppConfiguration.Default()
                 
