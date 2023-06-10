95f9401cab1bd38d4f55e9af1002780dce572e53
Tina Colvin <tinamcolvin11@gmail.com>
2023-06-10 17:44:04 -0600

    add json decoder for formatted datetime strings in Decoders.fs

--- cweed/Decoders.fs
+++ cweed/Decoders.fs
@@ -6,6 +6,26 @@ module ConfigurationDecoders
 
     open ConfigTypes
 
+    /// decodes the formatted date time from json. Caller needs to provide 
+    /// a fmt string that represents the DateTime valid string fmt used to 
+    /// create the encoded DateTime
+    let formattedDateTimeDecoder (fmt: string) : Decoder<System.DateTime> = 
+        fun path value -> 
+        if Decode.Helpers.isString value then
+            let v: Newtonsoft.Json.Linq.JValue = unbox value 
+            let tryDate = System.DateTime.TryParseExact(
+                v.ToString(),
+                fmt,
+                System.Globalization.CultureInfo.InvariantCulture,
+                System.Globalization.DateTimeStyles.None
+            )
+            match tryDate with 
+            | (true, dt) -> Ok dt
+            | (false, _) -> (path, BadField("DateTime parsing error", value)) |> Error
+        else 
+            (path, BadField("Datetime parsing error", value)) |> Error
+
+
     let browserConfigDecoder : Decoder<BrowserConfiguration> = 
         Decode.object (
             fun get -> 
