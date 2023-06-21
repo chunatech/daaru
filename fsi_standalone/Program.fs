7f99498730fe08ef330e870fcd1cbafec1e0fd8a
Tina Colvin <tinamcolvin11@gmail.com>
2023-06-21 17:02:33 -0600

    fix bad merge commit in fsi program file

--- fsi_standalone/Program.fs
+++ fsi_standalone/Program.fs
@@ -1,5 +1,4 @@
-<<<<<<< Updated upstream:fsi_standalone/Program.fs
-﻿open System.Globalization
+open System.Globalization
 open System.Text
 open System.IO
 open FSharp.Compiler.Interactive.Shell
@@ -66,78 +65,3 @@ let main argv =
     printfn "%s" fi.FullName
     evaluate fi.FullName
     0
-=======
-module Evaluator
-// Evauluator of the constructed/loaded .fsx files with a non-interactive FSI session
-
-open System.Globalization
-open System.Text
-open System.IO
-open FSharp.Compiler.Interactive.Shell
-
-// Init string builders to be used for output and error streams
-let sbOut: StringBuilder = StringBuilder()
-let sbErr: StringBuilder = StringBuilder()
-
-let fsi: FsiEvaluationSession =
-    let inStream: StringReader = new StringReader("")
-    let outStream: StringWriter = new StringWriter(sbOut)
-    let errStream: StringWriter = new StringWriter(sbErr)
-
-    try
-        // Create FSI evaluation session
-        // First arg in argv below may not be evaluated,
-        // however subsequent args will be.
-        // https://github.com/fsharp/fsharp-compiler-docs/issues/877
-            let fsiConfig: FsiEvaluationSessionHostConfig = FsiEvaluationSession.GetDefaultConfiguration()
-            let argv: string[] = [| "fsi.exe"; "--noninteractive" |]
-            FsiEvaluationSession.Create(fsiConfig, argv, inStream, outStream, errStream)
-    with
-    | (ex: exn) ->
-        printfn "Error: %A" ex
-        printfn "Inner: %A" ex.InnerException
-        printfn "ErrorStream: %s" (errStream.ToString())
-        raise ex
-
-
-let getOpen (path: string) =
-    let path: string = Path.GetFullPath path
-    let filename: string = Path.GetFileNameWithoutExtension path
-    let textInfo: TextInfo = (CultureInfo("en-US", false)).TextInfo
-    textInfo.ToTitleCase filename
-
-
-let getLoad (path: string) =
-        let path: string = Path.GetFullPath path
-        path.Replace("\\", "\\\\")
-
-
-let evaluate (path: string) =
-    let filename: string = getOpen path
-    let load: string = getLoad path
-
-    let _, (errs: FSharp.Compiler.Diagnostics.FSharpDiagnostic[]) = fsi.EvalInteractionNonThrowing(sprintf "#load \"%s\";;" load)
-    if errs.Length > 0 then printfn "Load Errors : %A" errs
-
-    let _, (errs: FSharp.Compiler.Diagnostics.FSharpDiagnostic[]) = fsi.EvalInteractionNonThrowing(sprintf "open %s;;" filename)
-    if errs.Length > 0 then printfn "Open Errors : %A" errs
-
-    // let (res: Choice<FsiValue option,exn>),(errs: FSharp.Compiler.Diagnostics.FSharpDiagnostic[]) = fsi.EvalExpressionNonThrowing(sprintf "map;;")
-    // if errs.Length > 0 then printfn "Get map Errors : %A" errs
-
-    let _, (errs: FSharp.Compiler.Diagnostics.FSharpDiagnostic[]) = fsi.EvalInteractionNonThrowing(sprintf "#quit;;")
-    if errs.Length > 0 then printfn "Quit Errors : %A" errs
-
-    // match res with
-    // | Choice1Of2 (Some (f: FsiValue)) ->
-    //     f.ReflectionValue :?> Transformation |> Some
-    // | _ -> None
-    ()
-
-
-let evalScript (path: string) =
-    task {
-        printfn $"Running %s{path}..."
-        evaluate path
-    }
->>>>>>> Stashed changes:cWeed/Evaluator.fs
