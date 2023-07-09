7b688e16cc202e3fcae5d945374aa183e454edb8
Chase Colvin <chaseacolvin@gmail.com>
2023-07-08 19:15:59 -0600

    Fix minor issues with both build scripts and several of the proj files

--- build.ps1
+++ build.ps1
@@ -10,8 +10,7 @@ $OUTDIR = Join-Path $PSScriptRoot "build/release/$BuildTarget/cweed"
 
 Write-Output "Removing old build file at: $OUTDIR"
 
-Remove-Item (Join-Path $OUTDIR 'cweed') -Force
-Remove-Item (Join-Path $OUTDIR 'cweed.pdb') -Force
+Remove-Item (Join-Path $OUTDIR 'cw*') -Force
 Remove-Item (Join-Path $OUTDIR 'fsi_standalone') -Recurse -Force
 
 if ($CleanBuild) {
@@ -30,23 +29,25 @@ catch {
 }
 
 [string[]]$folders1 = @('drivers', 'scripts', 'libs', 'config', 'templates')
-[string[]]$folders2 = @('staging', 'screenshots', 'logs')
+[string[]]$folders2 = @('staging', 'screenshots', 'logs', 'results')
 
 foreach ($folder in $folders1+$folders2) {
     New-Item -ItemType Directory -Path (Join-Path $OUTDIR $folder) -Force
 }
 
 foreach ($folder in $folders1) {
-    $children = @(Get-ChildItem (Join-Path $PSScriptRoot "external_resources/default_$($folder)"))
+    $extResFolder = "external_resources/default_$($folder)"
+    if ($folder -eq "drivers") { $extResFolder = Join-Path $extResFolder $BuildTarget }
+    $children = @(Get-ChildItem (Join-Path $PSScriptRoot $extResFolder))
     $outPathBase = Join-Path $OUTDIR $folder
 
     foreach ($child in $children) {
         $outPath = Join-Path $outPathBase (Split-Path $child.Fullname -Leaf)
 
         if (!(Test-Path $outPath) -or $CleanBuild) {
-            Copy-Item -Path $child.Fullname -Destination $outPathBase -Force
+            Copy-Item -Path $child.Fullname -Destination $outPathBase -Recurse -Force
         }
     }
 }
 
-Copy-Item -Recurse -Path (Join-Path $PSScriptRoot 'external_resources/default_results_processing/*') -Destination (Join-Path $OUTDIR 'results') -Force
+Copy-Item -Recurse -Path (Join-Path $PSScriptRoot 'external_resources/default_results_processing/*') -Destination (Join-Path $OUTDIR 'results/') -Force
--- build.sh
+++ build.sh
@@ -5,13 +5,10 @@ OUTDIR=./build/release/$1/cweed
 
 echo "Removing old build files at: ${OUTDIR}"
 
-rm $OUTDIR/cweed
-rm $OUTDIR/cweed.pdb
+rm $OUTDIR/cw*
 rm -rf $OUTDIR/fsi_standalone/
-rm -rf src/cweed/bin
-rm -rf src/fsi_standalone/bin
-rm -rf src/cweed/obj
-rm -rf src/fsi_standalone/obj
+rm -rf src/*/bin
+rm -rf src/*/obj
 
 dotnet publish ./src/cweed/cweed.fsproj -c Release -r $1 -o $OUTDIR --self-contained && \
 dotnet publish ./src/fsi_standalone/fsi_standalone.fsproj -c Release -r $1 -o $OUTDIR/fsi_standalone --self-contained && \
@@ -20,7 +17,7 @@ for d in config logs results scripts staging drivers libs screenshots templates;
     [ ! -d $OUTDIR/$d ] && mkdir $OUTDIR/$d && echo "Created dir ${OUTDIR}/${d}"
 done
 
-cp -prnv external_resources/default_drivers/* $OUTDIR/drivers/
+cp -prnv external_resources/default_drivers/$1/* $OUTDIR/drivers/
 cp -prnv external_resources/default_scripts/* $OUTDIR/scripts/
 cp -prnv external_resources/default_libs/* $OUTDIR/libs/
 cp -prnv external_resources/default_config/* $OUTDIR/config/
