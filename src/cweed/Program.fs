7ba4aa88ec31d74128a0b7de7df6cf10efbd18af
Tina Colvin <tinamcolvin11@gmail.com>
2023-07-13 15:13:41 -0600

    add logs and comments to main program file

--- src/cweed/Program.fs
+++ src/cweed/Program.fs
@@ -8,24 +8,37 @@ open System
 open System.IO
 open System.Reflection
 
+/// this is where cweed looks for the standalone fsi portion of the application 
 let fsiSaLocation: string = (Path.Join(AppContext.BaseDirectory, "fsi_standalone/fsi_standalone"))
+
+/// the staging directory location. see cweed.Transactions/cwTransactionsBuilder.fs
 let stagingDir: string = TransactionBuilder.stagingDir
 
+/// initialize the application configuration, logger and transaction builder.  
 let InitializeProgram () = 
     let mutable appConfig: AppConfiguration = ConfigFileHandler.readConfigFileOrDefault ()
     let logger: Logger = new Logger(appConfig.logs.FileSizeLimit, enum<Severity>(appConfig.logs.Severity))
     TransactionBuilder.init (appConfig) 
     appConfig, logger
 
+
 [<EntryPoint>]
 let main args = 
     // initialize configurations
     let (appConfig: AppConfiguration), (logger: Logger) = InitializeProgram()
+
+    // where the logs are stored, as given by the application configuration
     let logDirectory: string = appConfig.logs.LogDirectory
+
+    // the name of the application log file
     let cweedLogFile: string = Path.Join(logDirectory, "cweed.log")
 
+    logger.Log cweedLogFile (MethodBase.GetCurrentMethod()) Severity.Info $"application configuration received"
+    logger.Log cweedLogFile (MethodBase.GetCurrentMethod()) Severity.Info $"transaction builder initialzied"
+    logger.Log cweedLogFile (MethodBase.GetCurrentMethod()) Severity.Info $"logger initialized"
+
+    // if the log directory doesnt exist, create it
     if not <| Directory.Exists(logDirectory) then Directory.CreateDirectory(logDirectory) |> ignore
-    
     logger.Log cweedLogFile (MethodBase.GetCurrentMethod()) Severity.Info $"program initialzied with settings: {appConfig}"
 
     // register existing scripts 
@@ -41,13 +54,9 @@ let main args =
     let runner: TransactionRunner = TransactionRunner.init fsiSaLocation appConfig logger cweedLogFile
     logger.Log cweedLogFile (MethodBase.GetCurrentMethod()) Severity.Info $"transaction runner initialized"
 
-
+    // main loop of the program.. sleeps for 100ms, runs transactions, and then processes logs 
     while true do 
         Threading.Thread.Sleep(100)
         runner.runTransactions ()
         logger.ProcessQueue()
     exit 0
-
-(*
-    [] add ref to transaction lib and start integrating into main
-*)
